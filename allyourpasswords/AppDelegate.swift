a2df29c309fcee529705edd9a5769ef2cd69abc4
Sean Walker <swlkr.rbl@gmail.com>
2019-01-17 13:11:44 -0700

    Initial Commit

---
+++ allyourpasswords/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  allyourpasswords
+//
+//  Created by Sean Walker on 1/17/19.
+//  Copyright © 2019 Sean Walker. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ allyourpasswords/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  allyourpasswords
+//
+//  Created by Sean Walker on 1/17/19.
+//  Copyright © 2019 Sean Walker. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
